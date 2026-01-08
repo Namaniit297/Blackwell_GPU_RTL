813ea514607837a3e8c38ea7da521bf9776c5de4
Naman Kalra <ee23b032@iittp.ac.in>
2026-01-07 23:11:03 -0800

    Update sm_top.sv

--- rtl/SM/sm_top.sv
+++ rtl/SM/sm_top.sv
@@ -1,1 +1,9 @@
+// FILE: sm_top.sv
+// Streaming Multiprocessor top (instantiates SM box, thread scheduler, caches, etc)
+module sm_top #(parameter ID=0) (
+  input logic clk, rst_n
+);
+  // local structures
+  sm_box #(.ID(ID)) u_sm_box (.clk(clk), .rst_n(rst_n));
+endmodule
 
